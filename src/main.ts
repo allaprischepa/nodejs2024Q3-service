d7ba470ed37b1d5417724789e10065f55ec838a3
Alla Prishchepa <tyurina.alla@gmail.com>
2024-11-07 13:20:01 +0400

    feat: configure swagger and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const port = env.PORT || 4000;
+
+  // Create a Swagger document
+  const docPath = 'docs';
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(docPath, app, document);
+
+  await app.listen(port, () => {
+    console.log(`Application is running at: http://localhost:${port}`);
+    console.log(
+      `You can access the documentation at: http://localhost:${port}/${docPath}`,
+    );
+  });
 }
 bootstrap();
