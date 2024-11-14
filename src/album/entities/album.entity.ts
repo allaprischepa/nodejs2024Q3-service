c37cde552a3716cb3952084c9bdec3fbb90d1603
Alla Prishchepa <tyurina.alla@gmail.com>
2024-11-14 16:46:00 +0400

    feat: update artist entity

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,13 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { CreateAlbumDto } from '../dto/create-album.dto';
-import { v4 as uuidV4 } from 'uuid';
-
-interface Album {
-  id: string;
-  name: string;
-  year: number;
-  artistId: string | null;
-}
+import { Album } from '@prisma/client';
 
 export class AlbumEntity implements Album {
   @ApiProperty({ example: '7429cef1-0a5d-4fdd-9df4-2443d486d34a' })
@@ -21,13 +13,4 @@ export class AlbumEntity implements Album {
 
   @ApiProperty({ example: '6476c336-c646-4c28-bbea-7390b385518a' })
   artistId: string | null;
-
-  constructor(createAlbumDto: CreateAlbumDto) {
-    const { name, year, artistId } = createAlbumDto;
-
-    this.id = uuidV4();
-    this.name = name;
-    this.year = year;
-    this.artistId = artistId;
-  }
 }
